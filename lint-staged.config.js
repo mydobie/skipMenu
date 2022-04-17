51d270d007db2eafe6c3a666ea09f01dffdf9ffe
kimdobie <kimdobie@gmail.com>
2022-04-17 15:55:04 -0500

    add husky

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  '**/*.{ts,tsx,js,jsx}': [
+    (jsFiles) => jsFiles.map((jsFile) => `npm run lint:js ${jsFile}`), // check files
+  ],
+  '**/*.{scss,css}': [
+    (scssFiles) => scssFiles.map((scssFile) => `npm run lint:scss ${scssFile}`), // check files
+  ],
+  '**/*.{html,json,md}': [
+    (htmlFiles) => htmlFiles.map((htmlFile) => `npm run prettier ${htmlFile}`), // check files
+  ],
+};

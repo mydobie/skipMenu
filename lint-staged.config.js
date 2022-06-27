6f7d85eb0d4888ce3f892fd44230800d572584fe
kimdobie <kimdobie@gmail.com>
2022-06-26 21:50:35 -0500

    updated dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.{ts,tsx,js,jsx}': [
     (jsFiles) => jsFiles.map((jsFile) => `npm run lint:js ${jsFile}`), // check files
   ],
-  '**/*.{scss,css}': [
+  '**/*.{scss}': [
     (scssFiles) => scssFiles.map((scssFile) => `npm run lint:scss ${scssFile}`), // check files
   ],
   '**/*.{html,json,md}': [
